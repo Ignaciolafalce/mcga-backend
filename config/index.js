e66d92005aacaa935340c6235151eb265767169b
Ignacio Lafalce <nacho_lafalce@hotmail.com>
2020-08-10 00:02:06 -0300

    git ignore and env example

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
     DEV: process.env.NODE_ENV !== 'production',
     SERVER_PORT:process.env.PORT || 5000,
     SERVER_RESTART_AT_ms: 50000,
-    JWT_SECRET_KEY:process.env.JWT_SECRET_KEY || "Default".toString(),
+    JWT_SECRET_KEY:process.env.JWT_SECRET_KEY || "YourDefault".toString(),
     JWT_EXPIRES_IN:process.env.JWT_EXPIRES_IN,
     MONGODB_CONNECTION_URI:process.env.MONGODB_CONNECTION_URI
 }
