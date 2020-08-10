24bb15095e9dc01febffed818b281c1cd6e0e905
Ignacio Lafalce <nacho_lafalce@hotmail.com>
2020-08-10 00:45:51 -0300

    static frontend files build

---
+++ build/precache-manifest.667ee6a35f0e5f8e56fef7148e07228a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e0b8b05a756a4db80eec15f5eea27a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07b35f67839452c2f673",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b25443230117fe7f5b12",
+    "url": "/static/css/main.9a266540.chunk.css"
+  },
+  {
+    "revision": "07b35f67839452c2f673",
+    "url": "/static/js/2.30d3f8de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.30d3f8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25443230117fe7f5b12",
+    "url": "/static/js/main.c4d6b296.chunk.js"
+  },
+  {
+    "revision": "273800f8a5c2161d07b4",
+    "url": "/static/js/runtime-main.a3e9a530.js"
+  }
+]);
